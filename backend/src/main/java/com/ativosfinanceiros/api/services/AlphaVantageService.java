921479e17514174caaeb1495fa4cb9a06f21dd77
Jean Fernandine <jfernandine@gmail.com>
2023-09-10 18:46:50 -0300

    correcao injecao dependencia apikey

--- backend/src/main/java/com/ativosfinanceiros/api/services/AlphaVantageService.java
+++ backend/src/main/java/com/ativosfinanceiros/api/services/AlphaVantageService.java
@@ -1,23 +1,20 @@
 package com.ativosfinanceiros.api.services;
 
-import com.ativosfinanceiros.api.config.AVConfig;
 import com.ativosfinanceiros.api.entities.AlphaVantageResponse;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Service
 public class AlphaVantageService {
+
     @Value("${alphavantage.api.key}")
     private String apiKey;
-    @Autowired
-    private AVConfig config;
+
     private final WebClient.Builder webClientBuilder;
 
     @Autowired
@@ -27,9 +24,8 @@ public class AlphaVantageService {
 
     @Transactional(readOnly = true)
     public Mono<AlphaVantageResponse> getStockData(String function, String symbol) {
-        String apiKey = config.getApiKey();
-        String apiUrl = "https://www.alphavantage.co/query?function=" + function + "&symbol=" + symbol + "&apikey=demo";
-        // + apiKey;
+        String apiUrl = "https://www.alphavantage.co/query?function=" + function + "&symbol=" + symbol + "&apikey="
+         + apiKey;
 
         return webClientBuilder.baseUrl(apiUrl)
                 .build()
